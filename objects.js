b7851473f7d61ead534f86251176fdfb8499eb19
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 22:14:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {key:'value'};
 
 function updatePlaylist (playlist,artistName,songTitle){
-  playlist.Phil Ochs= "Here's to the State of Mississippi"
+  playlist['Phil Ochs']= "Here's to the State of Mississippi"
   
 
   return playlist;
