85b1b4f970c9e0a622baeabb49d3912b9a0ba9e4
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 22:10:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,14 @@
 var playlist = {key:'value'};
 
 function updatePlaylist (playlist,artistName,songTitle){
-  playlist["Phil Ochs"] = "Here's to the State of Mississippi";
+  playlist.artistName = Phil Ochs;
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
 
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
-delete playlist['Phil Ochs'];
+delete playlist.artistName;
 return playlist;
 
 }
