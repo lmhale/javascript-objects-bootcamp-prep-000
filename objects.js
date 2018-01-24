ab47776d8d12dfda36d39689ea651298a0cbed3b
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 22:36:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,1 @@
-var playlist = {artistName:'songTitle'};
-
-function updatePlaylist (playlist,artistName,songTitle){
-  playlist['Phil Ochs']= "Here's to the State of Mississippi"
-
-
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName) {
-delete playlist.['Phil Ochs'];
-
-return playlist;
-
-}
+var playlist = new object ({artistName:'songTitle'});
